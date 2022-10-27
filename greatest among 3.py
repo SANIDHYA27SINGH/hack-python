bc2aab1c8993cc29dfa2c717667ce4f77b6c1550
Divyanshu Singh <33370927+Div25singh@users.noreply.github.com>
2022-10-27 20:03:12 +0530

    Delete greatest among 3.py

--- greatest among 3.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter a"));
-b=int(input("enter b"));
-c=int(input("enter c"));
-if a>b and a>c:
-	print("a is the largest");
-elif b>a and b>c:
-	print("b is the largest");
-else:
-	print("c is the largest");
